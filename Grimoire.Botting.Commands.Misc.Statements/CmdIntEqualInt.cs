4a9ef939a12cf06b736ddcd5ebe96593c2c635aa
wispie <62823280+wispie@users.noreply.github.com>
2021-04-05 02:00:04 +0700

    Create CmdIntEqualInt.cs

---
+++ Grimoire.Botting.Commands.Misc.Statements/CmdIntEqualInt.cs
@@ -0,0 +1,28 @@
+using Grimoire.Game;
+using System.Threading.Tasks;
+
+namespace Grimoire.Botting.Commands.Misc.Statements
+{
+    public class CmdIntEqualInt : StatementCommand, IBotCommand
+    {
+        public CmdIntIs()
+        {
+            Tag = "Misc";
+            Text = "Int is equal to Int";
+        }
+
+        public Task Execute(IBotEngine instance)
+        {
+            if (Configuration.Tempvalues[Value1] == Configuration.Tempvalues[Value2])
+            {
+                instance.Index++;
+            }
+            return Task.FromResult<object>(null);
+        }
+
+        public override string ToString()
+        {
+            return $"{Value1} == {Value2}";
+        }
+    }
+}
