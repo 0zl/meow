8934b93bb5462295803c779f2c1b929cbecf2fdb
wispie <62823280+wispie@users.noreply.github.com>
2021-04-19 02:59:13 +0700

    Revert "Update app.config"
    This reverts commit 508a1eca3a6a35d4e2539771003bae1f3b96529e.

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -22,5 +22,17 @@ namespace Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public string Style {
+            get {
+                return ((string)(this["Style"]));
+            }
+            set {
+                this["Style"] = value;
+            }
+        }
     }
 }
