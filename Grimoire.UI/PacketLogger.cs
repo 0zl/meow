1af2944e2cd28bce9a681385d3d68e3307eedc60
wispie <62823280+wispie@users.noreply.github.com>
2021-05-01 13:22:17 +0700

    PacketLogger scrollbar

--- Grimoire.UI/PacketLogger.cs
+++ Grimoire.UI/PacketLogger.cs
@@ -87,105 +87,106 @@ namespace Grimoire.UI
 
         private void InitializeComponent()
         {
-            ComponentResourceManager resources = new ComponentResourceManager(typeof(PacketLogger));
-            this.txtPackets = new DarkTextBox();
-            this.btnStart = new DarkButton();
-            this.btnStop = new DarkButton();
-            this.btnCopy = new DarkButton();
-            this.btnClear = new DarkButton();
-            this.tableLayoutPanel1 = new TableLayoutPanel();
+            this.txtPackets = new DarkUI.Controls.DarkTextBox();
+            this.btnStart = new DarkUI.Controls.DarkButton();
+            this.btnStop = new DarkUI.Controls.DarkButton();
+            this.btnCopy = new DarkUI.Controls.DarkButton();
+            this.btnClear = new DarkUI.Controls.DarkButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtPackets
             // 
-            this.txtPackets.Anchor = AnchorStyles.Top | AnchorStyles.Bottom
-            | AnchorStyles.Left
-            | AnchorStyles.Right;
+            this.txtPackets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtPackets.Location = new System.Drawing.Point(12, 12);
+            this.txtPackets.MaxLength = 2147483647;
             this.txtPackets.Multiline = true;
             this.txtPackets.Name = "txtPackets";
-            this.txtPackets.Size = new System.Drawing.Size(420, 160);
+            this.txtPackets.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPackets.Size = new System.Drawing.Size(422, 244);
             this.txtPackets.TabIndex = 15;
             // 
             // btnStart
             // 
-            this.btnStart.Dock = DockStyle.Fill;
+            this.btnStart.Checked = false;
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnStart.Location = new System.Drawing.Point(318, 3);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(99, 24);
+            this.btnStart.Size = new System.Drawing.Size(101, 24);
             this.btnStart.TabIndex = 16;
             this.btnStart.Text = "Start";
-            //this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new EventHandler(this.btnStart_Click);
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnStop
             // 
-            this.btnStop.Dock = DockStyle.Fill;
+            this.btnStop.Checked = false;
+            this.btnStop.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnStop.Location = new System.Drawing.Point(213, 3);
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(99, 24);
             this.btnStop.TabIndex = 17;
             this.btnStop.Text = "Stop";
-            //this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new EventHandler(this.btnStop_Click);
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // btnCopy
             // 
-            this.btnCopy.Dock = DockStyle.Fill;
+            this.btnCopy.Checked = false;
+            this.btnCopy.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnCopy.Location = new System.Drawing.Point(108, 3);
             this.btnCopy.Name = "btnCopy";
             this.btnCopy.Size = new System.Drawing.Size(99, 24);
             this.btnCopy.TabIndex = 18;
             this.btnCopy.Text = "Copy";
-            //this.btnCopy.UseVisualStyleBackColor = true;
-            this.btnCopy.Click += new EventHandler(this.btnCopy_Click);
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
             // 
             // btnClear
             // 
-            this.btnClear.Dock = DockStyle.Fill;
+            this.btnClear.Checked = false;
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnClear.Location = new System.Drawing.Point(3, 3);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(99, 24);
             this.btnClear.TabIndex = 19;
             this.btnClear.Text = "Clear";
-            //this.btnClear.UseVisualStyleBackColor = true;
-            this.btnClear.Click += new EventHandler(this.btnClear_Click);
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left
-            | AnchorStyles.Right;
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.Controls.Add(this.btnClear, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.btnStart, 3, 0);
             this.tableLayoutPanel1.Controls.Add(this.btnStop, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.btnCopy, 1, 0);
-            this.tableLayoutPanel1.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 175);
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 259);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(420, 30);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(422, 30);
             this.tableLayoutPanel1.TabIndex = 20;
             // 
             // PacketLogger
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 207);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 291);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.txtPackets);
-            this.Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            this.Icon = global::Properties.Resources.GrimoireIcon;
             this.MinimizeBox = false;
             this.Name = "PacketLogger";
             this.Text = "Packet logger";
             this.TopMost = true;
-            this.FormClosing += new FormClosingEventHandler(this.PacketLogger_FormClosing);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PacketLogger_FormClosing);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
